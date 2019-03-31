9aca6f55307c3468eb2cc2390111af3f7f2d843c
Chuck Atkins <chuck.atkins@kitware.com>
2019-03-31 02:45:16 -0400

    First commit

---
+++ scripts/aaargh_gray-scott.cmake
@@ -0,0 +1,6 @@
+set(CTEST_BUILD_NAME "MiniApp-Gray-Scott")
+set(dashboard_cache "
+ADIOS2_EXTERNAL_TESTING_GRAYSCOTT:BOOL=ON
+")
+
+include(${CMAKE_CURRENT_LIST_DIR}/aaargh_common.cmake)
---
+++ scripts/aaargh_pio.cmake
@@ -0,0 +1,6 @@
+set(CTEST_BUILD_NAME "ParallelIO")
+set(dashboard_cache "
+ADIOS2_EXTERNAL_TESTING_PIO:BOOL=ON
+")
+
+include(${CMAKE_CURRENT_LIST_DIR}/aaargh_common.cmake)
