97efd8c0330c0e0e3d5c44ab59c9ed84de05dce7
Norbert Podhorszki <pnorbert@ornl.gov>
2020-05-29 16:17:04 -0400

    Added test for LAMMPS to LAMMPS coupling for PCA analysis. Created by Lipeng Wan. This test uses the actual LAMMPS code.

---
+++ performance/lammps-pca/source_sw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add Cheetah to PATH
+export PATH=/gpfs/alpine/world-shared/csc143/sw/cheetah/bin:$PATH
+
+# Add tutorial cheetah campaign spec file path to PYTHONPATH
+export PYTHONPATH=/gpfs/alpine/world-shared/csc143/sw/cheetah:$PYTHONPATH
+
+# Add current directory to PYTHONPATH
+export PYTHONPATH=/gpfs/alpine/proj-shared/csc143/lwan86/cheetah-lammps:$PYTHONPATH
+
+# Add bpls to PATH
+# export PATH=/gpfs/alpine/world-shared/csc299/cheetah-handson-ecp-am-2020/sw/adios2-master-02.02.20/gcc/8.1.1/bin/:$PATH
+
