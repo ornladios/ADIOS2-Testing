1e50671f8d9cb62ff9cafcaa39555929aabc751e
Xin Liang <xinliang@rhea-login3g.ccs.ornl.gov>
2020-06-30 16:30:33 -0400

    add tau on rhea

--- performance/nwchem-sorting/rhea_sweep_groups.py
+++ performance/nwchem-sorting/rhea_sweep_groups.py
@@ -8,8 +8,8 @@ engines                 = [ {'BP4':{'OpenTimeoutSecs':'30.0'}}, {'SST':{}}, {"Nu
 adios_xml_file          = 'copro.xml'
 config_file             = ['copro-80.txt']
 run_repetitions         = 1
-batch_job_timeout_secs  = 3600
-per_experiment_timeout  = 600
+batch_job_timeout_secs  = 600
+per_experiment_timeout  = 100
 
 
 # node_layouts = summit_node_layouts('writer', 'reader')
