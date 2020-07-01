cfe787763bcb16dac4903303bd2aea2543a51f60
Xin Liang <xinliang@login3.summit.olcf.ornl.gov>
2020-07-01 11:12:37 -0400

    fix issues on summit

--- performance/nwchem-sorting/create_campaign_summit.sh
+++ performance/nwchem-sorting/create_campaign_summit.sh
@@ -4,5 +4,5 @@ export PYTHONPATH=$PYTHONPATH:$PWD
 directory=$PROJWORK/csc143/xin/adios_iotest_nwchem_copro
 touch $directory
 rm -rf $directory
-cheetah create-campaign -e campaign_summit.py -m rhea -o $directory -a $PWD
+cheetah create-campaign -e campaign_summit.py -m summit -o $directory -a $PWD
 
