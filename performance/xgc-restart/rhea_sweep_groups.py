e8011165bbbdb7a12fc6807f0223a5b4bc1db885
Dmitry Ganyushin <ganyushin@gmail.com>
2020-07-22 16:54:37 -0400

    changed node layout

--- performance/xgc-restart/rhea_sweep_groups.py
+++ performance/xgc-restart/rhea_sweep_groups.py
@@ -1,6 +1,5 @@
 from codar.savanna.machines import SummitNode
 from sweep_groups_helper import create_sweep_groups
-from summit_node_layouts import summit_node_layouts
 # Parameters 64, 256, 1024 nodes  16 processes per node
 writer_np               = [1024, 4096, 16384]
 reader_np_ratio         = [1]
@@ -11,13 +10,12 @@ run_repetitions         = 0
 batch_job_timeout_secs  = 3600
 per_experiment_timeout  = 600
 
-node_layouts = list()
-node_layouts.append([{'writer':16}, {'reader': 16}])
-n = SummitNode()
-for i in range(writers_per_node):
-    n.cpu[i] = "{}:{}".format("writer", i)
-    n.cpu[i + writers_per_node] = "{}:{}".format("reader", i)
-node_layouts = list(n)
+#n = SummitNode()
+#for i in range(writers_per_node[0]):
+#    n.cpu[i] = "{}:{}".format("writer", i)
+#    n.cpu[i + writers_per_node[0]] = "{}:{}".format("reader", i)
+node_layouts = []
+node_layouts.append([ {'writer': writers_per_node[0]}, {'reader': writers_per_node[0]}])
 sweep_groups = create_sweep_groups ('rhea',
                                     writer_np,
                                     reader_np_ratio,
