acbd347cc4dc60d9440d57e83c1a04fbe1220827
Charles Atkins <atkins3@login1.ascent.olcf.ornl.gov>
2020-09-15 16:37:52 -0400

    Made summit scripts more generic

---
+++ performance/xgc-restart/summit_tiny_sweep_groups.py
@@ -0,0 +1,30 @@
+from codar.savanna.machines import SummitNode
+from sweep_groups_helper import create_sweep_groups
+
+# Parameters 4 nodes for a night run
+writer_np               = [24]
+reader_np_ratio         = [1]
+writers_per_node_summit = [6]
+size_per_pe             = []
+engines                 = ['bp4', 'hdf5']
+run_repetitions         = 4
+batch_job_timeout_secs  = 3600
+per_experiment_timeout  = 600
+
+n = SummitNode()
+for i in range(writers_per_node_summit[0]):
+   n.cpu[i] = "{}:{}".format("writer", i)
+   n.cpu[i + writers_per_node_summit[0]] = "{}:{}".format("reader", i)
+
+node_layouts = [[n]]
+
+sweep_groups = create_sweep_groups ('summit',
+                                    writer_np,
+                                    reader_np_ratio,
+                                    size_per_pe,
+                                    engines,
+                                    node_layouts,
+                                    run_repetitions,
+                                    batch_job_timeout_secs,
+                                    per_experiment_timeout)
+
